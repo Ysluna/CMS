4cc0744ecd96ef96a003de64a7a5af0aeb11f1fe
Yamila <soledadluna.ysl@gmail.com>
2023-08-27 17:33:18 +0200

    nueva rama con react y sass

---
+++ dashboard react/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { PrimerComponente } from './components/PrimerComponente'
+
+function App() {
+  return (
+    <>
+      
+      <PrimerComponente />
+
+    </>
+  )
+}
+
+export default App
