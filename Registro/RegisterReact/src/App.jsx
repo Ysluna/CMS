7497d96deb21590d1373857b858612d896ed35bc
Zoe Carida <Zowycar@gmail.com>
2023-08-28 01:06:04 -0300

    +

--- Registro/RegisterReact/src/App.jsx
+++ Registro/RegisterReact/src/App.jsx
@@ -4,10 +4,8 @@ import UserRegister from './components/UserRegister'
 function App() {
   return (
     <>
-    <div>
-    <h1> ¡Bienvenidos! </h1>
-    <UserRegister />
-   </div>
+     <h1> ¡Bienvenidos! </h1>
+      <UserRegister />
     </>
   )
 }
