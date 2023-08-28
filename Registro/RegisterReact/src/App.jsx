21d4420da5dfe7c82cf1183959b7fc2ab4e9bb11
Zoe Carida <Zowycar@gmail.com>
2023-08-28 00:57:35 -0300

    Registro/React

---
+++ Registro/RegisterReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import UserRegister from './components/UserRegister'
+
+function App() {
+  return (
+    <>
+    <div>
+    <h1> ¡Bienvenidos! </h1>
+    <UserRegister />
+   </div>
+    </>
+  )
+}
+
+export default App
